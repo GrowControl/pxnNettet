a1008961cf8b7cbca99286a028035f9098080b82
lorenzop <lorenzo@poixson.com>
2017-11-12 14:54:52 -0500

    added ChannelState enum

---
+++ core/src/com/poixson/nettet/ChannelState.java
@@ -0,0 +1,38 @@
+package com.poixson.nettet;
+
+
+public enum ChannelState {
+
+	CLOSED     (0),
+	CONNECTING (1),
+	LISTENING  (2),
+	CONNECTED  (3);
+
+
+
+	public final int index;
+
+	ChannelState(final int index) {
+		this.index = index;
+	}
+
+
+
+	@Override
+	public String toString() {
+		switch (this) {
+		case CLOSED:
+			return "Closed";
+		case CONNECTING:
+			return "Connecting";
+		case LISTENING:
+			return "Listening";
+		case CONNECTED:
+			return "Connected";
+		}
+		return null;
+	}
+
+
+
+}
