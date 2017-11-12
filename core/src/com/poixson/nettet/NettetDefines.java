6718cbb6c4050baa19b7434ab999d598398b3983
lorenzop <lorenzo@poixson.com>
2017-11-12 14:55:28 -0500

    added NettetDefines class

---
+++ core/src/com/poixson/nettet/NettetDefines.java
@@ -0,0 +1,14 @@
+package com.poixson.nettet;
+
+
+public final class NettetDefines {
+	private NettetDefines() {}
+
+
+	public static final int MAX_PORT_NUMBER = 65534;
+	public static final int MAX_BACKLOG     = 100;
+	public static final int DEFAULT_BACKLOG = 10;
+	public static final int DEFAULT_TIMEOUT = 5000;
+
+
+}
