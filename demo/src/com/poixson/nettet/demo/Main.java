25c575f0a47295aa325138f1249c6a183da3bdf5
lorenzop <lorenzo@poixson.com>
2017-11-12 13:39:42 -0500

    added demo Main class (mostly empty)

---
+++ demo/src/com/poixson/nettet/demo/Main.java
@@ -0,0 +1,41 @@
+package com.poixson.nettet.demo;
+
+import com.poixson.nettet.PipelineFactory;
+import com.poixson.nettet.pipelines.PipelineDump;
+import com.poixson.nettet.pipelines.PipelineJSONStream;
+import com.poixson.nettet.pipelines.PipelineString;
+import com.poixson.utils.Keeper;
+import com.poixson.utils.ShellArgsTool;
+
+
+public class Main {
+
+	@SuppressWarnings("unused")
+	private static final Keeper keeper = Keeper.get();
+
+
+
+	public static void main(final String[] argsArray) {
+		// process shell arguments
+		final ShellArgsTool argsTool = ShellArgsTool.init(argsArray);
+
+		final PipelineFactory pipeFactory = new PipelineFactory() {
+			@Override
+			public void doInitPipes() {
+				this.addLast(
+					new PipelineString(),
+					new PipelineJSONStream(true),
+					new PipelineDump()
+				);
+			}
+		};
+		pipeFactory.init();
+//TODO:
+		
+		
+		
+	}
+
+
+
+}
