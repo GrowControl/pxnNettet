b0ae1c8bf962ce9abd0d069945b92c877d0a061b
lorenzop <lorenzo@poixson.com>
2017-12-13 16:29:47 -0500

    updated load libraries lines in Main class

--- demo/src/com/poixson/nettet/demo/Main.java
+++ demo/src/com/poixson/nettet/demo/Main.java
@@ -1,7 +1,8 @@
 package com.poixson.nettet.demo;
 
-import com.poixson.nettet.NettetLibraryLoader;
 import com.poixson.utils.Keeper;
+import com.poixson.utils.NativeAutoLoader;
+import com.poixson.utils.NativeAutoLoader.AutoMode;
 
 
 public class Main {
@@ -19,9 +20,16 @@ public class Main {
 //		final ShellArgsTool argsTool = ShellArgsTool.Init(argsArray);
 		// load libraries
 		{
-			// load unix socket library
-			NettetLibraryLoader.get()
-				.loadUnixSocketLibrary();
+			final NativeAutoLoader loader =
+					NativeAutoLoader.getNew(AutoMode.AUTO_MODE_SELF_CONTAINED)
+						.setDefaults(Main.class)
+						.setLocalLibPath("lib")
+						.setResourcesPath("lib/linux64");
+				// load unix socket library
+				loader.clone()
+//TODO: fix this path
+					.setResourcesPath("lib/amd64-Linux-gpp/jni")
+					.load("libjunixsocket-native-1.0.2.so");
 		}
 	}
 
